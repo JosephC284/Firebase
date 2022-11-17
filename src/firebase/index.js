62ca7dc2b253638a01943bebf3b7d47efd1f02eb
[JosephC282] <jchang284@gmail.com>
2022-11-17 13:48:50 -0500

    Created firebase.

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDri6SuqJKq5C7XrXeN9vR3MLYiRVZS9lY",
+  authDomain: "tmdb-4db21.firebaseapp.com",
+  projectId: "tmdb-4db21",
+  storageBucket: "tmdb-4db21.appspot.com",
+  messagingSenderId: "991750877254",
+  appId: "1:991750877254:web:d0b3b5edf31e8d936bda47"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
